6c3fd033a309d7039c2ef68b5670e3c00b36503a
srinivasagatla@lyrostech.com <srinivasareddygatla09@gmail.com>
2024-03-12 17:08:44 +0530

    use REducer hook

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App19 from "./App19";
+import App20 from "./App20";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-   <App19/>
+   <App20/>
   </>
 );
 
